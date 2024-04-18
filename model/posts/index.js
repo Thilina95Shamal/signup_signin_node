453d5e5bafeb3e50c1d8ae670370607fbe8ff683
ThilinaShamal <thilinashamal19@gmail.com>
2024-04-18 18:23:23 +0530

    Revert "Initial Code Commit"
    This reverts commit 205641929645d053f58b8251f2f1fca5a878a1b4.

--- model/posts/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const postSchema = require("./posts_schema"); 
-
-const posts = mongoose.model("posts", postSchema);
-
-module.exports = posts;
--- model/systemuser/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = require("./systemuser_schema"); 
-
-const user = mongoose.model("users", userSchema);
-
-module.exports = user;
